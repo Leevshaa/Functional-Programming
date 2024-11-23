74d7579d6877f7e44b0d702757ceedb8ab776d5a
Vitalii <vitaliti.yurchenko@gmail.com>
2024-11-23 19:24:45 +0200

    Updated randomizer, added edits in Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,8 @@
 package app;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,20 +15,37 @@ public class Main {
             }
         };
 
-        int result = addition.operate(4, 10);
-        System.out.println("Результат роботи функціонального інтерфейсу MathOperation: " + result +
-                "(додавання).");
-
         String inputData = "Тестовий рядок";
+
+        int result = addition.operate(4, 10);
+        System.out.println("\nРезультат роботи функціонального інтерфейсу MathOperation: " + result +
+                " (додавання).");
 
         StringManipulator lineToUpperCase = line -> line.toUpperCase();
         String result2 = lineToUpperCase.manipulate(inputData);
         System.out.println("Результат роботи функціонального інтерфейсу StringManipulator: " +
-                result2 + "(рядок перетворюється у верхній регістр).");
+                result2 + " (рядок перетворюється у верхній регістр).");
 
 
         int result3 = StringListProcessor.getCountUppercase(inputData);
         System.out.println("Результат роботи класу StringListProcessor: " + result3 +
-                "(відображається кількість великих літер у рядку).");
+                " (відображається кількість великих літер у рядку).");
+
+        Function<String, Integer> uppercaseCounter = StringListProcessor::getCountUppercase;
+        int result4 = uppercaseCounter.apply(inputData);
+        System.out.println("Результат роботи класу StringListProcessor через посилання" +
+                " на його метод у функції uppercaseCounter: " + result4 +
+                " (відображається кількість великих літер у рядку).");
+
+
+        try {
+            Supplier<Integer> randomNumber = () -> RandomNumberGenerator
+                    .getGenerateRandomNumber(0, 100000);
+            int result5 = randomNumber.get(); // постачальник
+            System.out.println("Результат роботи класу RandomNumberGenerator: " + result5 +
+                    " (отримуємо рандомне число у визначено діапазоні).");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Помилка в аргументах функції. Мінімальне число більше максимального!");
+        }
     }
 }
--- src/app/RandomNumberGenerator.java
+++ src/app/RandomNumberGenerator.java
@@ -3,6 +3,14 @@ package app;
 public class RandomNumberGenerator {
 
     private static int generateRandomNumber(int min, int max) {
+
+        if (min > max) {
+            throw new IllegalArgumentException("Мінімальне число більше за максимальне");
+        }
         return (int) (Math.random() * (max - min + 1)) + min;
+    }
+
+    public static int getGenerateRandomNumber(int min, int max) {
+        return generateRandomNumber(min, max);
     }
 }
